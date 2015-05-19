26ae90af9fd544e698b3c9e63b1cdd3610e98a04
Robert Laine-Wyllie <rlainewyllie@gmail.com>
2015-05-19 23:14:13 +1000

    created route to login page. Needs work

--- Silverado/Views/login.php
+++ Silverado/Views/login.php
@@ -5,8 +5,11 @@
 
 	<main class="wrapper yellow-bg round-first">
 		<h2>Checkout</h2>
-
-			<form method="POST" action="" class="login-form">
+		<?php
+		//search db for existing user, if user exists set session with their details and assign booking to their account
+		//else, add new user and set booking to their account
+		?>
+			<form method="POST" action="/checkout" class="login-form">
 				<input type="text" placeholder="User Name" required>
 				<input type="password" placeholder="Password" required>
 			</form>
