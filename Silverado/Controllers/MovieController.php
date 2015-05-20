5841e742887fa15aad517cdc3efec93df317ea35
Andre Ribas <andreribas@gmail.com>
2015-05-20 12:31:00 +1000

    Fix the MovieController->movie action.

--- Silverado/Controllers/MovieController.php
+++ Silverado/Controllers/MovieController.php
@@ -10,8 +10,13 @@ class MovieController extends AbstractController
 
 	protected $movies;
 
-
 	public function __construct($httpRequest = [], $args = [])
+	{
+
+
+	}
+
+	public function movie($httpRequest = [], $args = [])
 	{
 
 		$this->movies = MovieModel::getAll();
