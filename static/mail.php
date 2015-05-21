d68aaca0077615da8d9f81bd4c8e2ea6c6bca9da
Robert Laine-Wyllie <rlainewyllie@gmail.com>
2015-05-22 00:44:33 +1000

    added header to the mail script

--- static/mail.php
+++ static/mail.php
@@ -1,9 +1,10 @@
 <?php
 #Automated mailing script. Only for demo. Do not leave active. Could result in spam flagging.
-$name='';
-$address='';
+$to='';
 $subject='';
+$name='';
 $message='';
+$header = 'From: webmaster@silverado.com';
 
 if(isset($_POST) && !empty($_POST)) {
   if (isset($_POST['name'])) {
@@ -11,7 +12,7 @@ if(isset($_POST) && !empty($_POST)) {
   }
 
   if (isset($_POST['email']) && ($_POST['test-email'] == true)) {
-    $adress = $_POST['email'];
+    $to = $_POST['email'];
   }
 
   if (isset($_POST['subject'])) {
@@ -22,8 +23,8 @@ if(isset($_POST) && !empty($_POST)) {
     $message = "Dear " . $name . ",
       " . $_POST['message'];
   }
-  mail($address, $subject, $message);
 }
 
+mail($to, $subject, $message, $header);
 
 ?>
