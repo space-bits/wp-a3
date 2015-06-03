efbeeffcf99f52e57f33d11680929c1d92ed0618
Robert Laine-Wyllie <rlainewyllie@gmail.com>
2015-06-03 13:39:45 +1000

    changed singl-elogin to post to login

--- Silverado/Views/inc/single-login.php
+++ Silverado/Views/inc/single-login.php
@@ -1,4 +1,4 @@
-<form method="POST" action="/checkout" class="login-form">
+<form method="POST" action="<?= getBaseUri();?>login" class="login-form">
     <p>Already have an account?</p>
     <input name="username" type="text" placeholder="User Name" required>
     <input name="password" type="password" placeholder="Password" required>
