9c22ee0ece8009da2d689ed161736aee1895d5ba
Robert Laine-Wyllie <rlainewyllie@gmail.com>
2015-06-05 01:09:49 +1000

    Added logic to display particular seats only if booked.

--- Silverado/Views/ticket.php
+++ Silverado/Views/ticket.php
@@ -4,7 +4,7 @@ include_once('inc/header.php');
 ?>
 
 <main class="wrapper yellow-bg round-first">
-	<h2>Ticket</h2>
+	<h2>Tickets</h2>
 	<?php foreach ($_SESSION['cart'] as $id => $booking) { ?>
 		<div class="ticket">
 			<h3><?= $booking->screening->time; ?> <?= $booking->screening->day; ?></h3>
@@ -17,32 +17,54 @@ include_once('inc/header.php');
 				<h4>Your Seats</h4>
 					<ul>
 						<?php
-						if($booking->sa != 0){
-							echo '<li>';
-							$booking->sa; echo 'SA';
+						if(($booking->sa) != 0){
+							echo '<li>'; ?>
+							<?= $booking->sa; ?>
+							<?php echo 'SA';
+							echo '</li>';
+						}
+						if(($booking->sp) != 0){
+							echo '<li>'; ?>
+							<?= $booking->sp; ?>
+							<?php echo 'SP';
+							echo '</li>';
+						}
+						if(($booking->sc) != 0){
+							echo '<li>'; ?>
+							<?= $booking->sc; ?>
+							<?php echo 'SC';
+							echo '</li>';
+						}
+						if(($booking->fa) != 0){
+							echo '<li>'; ?>
+							<?= $booking->fa; ?>
+							<?php echo 'FA';
+							echo '</li>';
+						}
+						if(($booking->fc) != 0){
+							echo '<li>'; ?>
+							<?= $booking->fc; ?>
+							<?php echo 'FC';
+							echo '</li>';
+						}
+						if(($booking->b1) != 0){
+							echo '<li>'; ?>
+							<?= $booking->b1; ?>
+							<?php echo 'B1';
+							echo '</li>';
+						}
+						if(($booking->b2) != 0){
+							echo '<li>'; ?>
+							<?= $booking->b2; ?>
+							<?php echo 'B2';
+							echo '</li>';
+						}
+						if(($booking->b3) != 0){
+							echo '<li>'; ?>
+							<?= $booking->b3; ?>
+							<?php echo 'B3';
 							echo '</li>';
 						} ?>
-						<li>
-							<?= $booking->sp; ?> SP
-						</li>
-						<li>
-							<?= $booking->sc; ?> SC
-						</li>
-						<li>
-							<?= $booking->fa; ?> FA
-						</li>
-						<li>
-							<?= $booking->fc; ?> FC
-						</li>
-						<li>
-							<?= $booking->b1; ?> B1
-						</li>
-						<li>
-							<?= $booking->b2; ?> B2
-						</li>
-						<li>
-							<?= $booking->b3; ?> B3
-						</li>
 					</ul>
 				</ul>
 			</div>
